76b5da418267dd5e9f27cb893a0ebd2bf092810a
Irina02Mihailova <imihailova193@gmail.com>
2024-11-04 18:03:01 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-Print("тест")
+number = int(input("Введите число: "))
+while number != 0:
+    if number % 2 == 0:
+        print(f"Число {number} является четным ")
+    else:
+        print(f"Число {number} не является четным ")
+    number = int(input("Введите число: "))
+
